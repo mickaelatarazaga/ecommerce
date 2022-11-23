74be727e4392a521ce53fd05298a0c969b6ec17a
Mickaela Tarazaga <94411297+mickaelatarazaga@users.noreply.github.com>
2022-11-23 13:50:48 -0300

    Add product entity

---
+++ products/src/main/java/com/mtarazaga/products/model/entity/Product.java
@@ -0,0 +1,22 @@
+package com.mtarazaga.products.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
