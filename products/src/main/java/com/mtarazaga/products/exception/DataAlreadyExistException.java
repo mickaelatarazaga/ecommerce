c8fe2eb17c2c249186d8507cc7eee114ec76aef1
Mickaela Tarazaga <94411297+mickaelatarazaga@users.noreply.github.com>
2022-11-25 10:29:47 -0300

    Add DataAlreadyExistException

---
+++ products/src/main/java/com/mtarazaga/products/exception/DataAlreadyExistException.java
@@ -0,0 +1,11 @@
+package com.mtarazaga.products.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class DataAlreadyExistException extends RuntimeException {
+    public DataAlreadyExistException(String message) {
+        super(message);
+    }
+}
