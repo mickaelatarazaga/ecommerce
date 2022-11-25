e930e40b8d2ae23bebea150ccbbf20abbc3a38ae
Mickaela Tarazaga <94411297+mickaelatarazaga@users.noreply.github.com>
2022-11-25 10:29:26 -0300

    Add NotFoundException

---
+++ products/src/main/java/com/mtarazaga/products/exception/NotFoundException.java
@@ -0,0 +1,13 @@
+package com.mtarazaga.products.exception;
+
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@NoArgsConstructor
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String msg) {
+        super(msg);
+    }
+}
