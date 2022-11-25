fb5453a8c11e0c0b2e9566efbe51609762d1664b
Mickaela Tarazaga <94411297+mickaelatarazaga@users.noreply.github.com>
2022-11-25 10:30:45 -0300

    Update Product.java

--- products/src/main/java/com/mtarazaga/products/model/entity/Product.java
+++ products/src/main/java/com/mtarazaga/products/model/entity/Product.java
@@ -4,19 +4,36 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@SQLDelete(sql = "UPDATE users SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")
 @Entity
-public class Product {
+@Table(name = "products")
+public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private BigDecimal price;
+    @Column(name = "creation_date")
+    @CreationTimestamp
+    private Date creationDate;
+    private boolean deleted = Boolean.FALSE;
 }
